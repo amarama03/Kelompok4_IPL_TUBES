00a4ae1da0249a6e3b04e4c96c44934b9acf6754
Zorojuro213 <147685699+Zorojuro213@users.noreply.github.com>
2024-01-01 10:17:01 +0700

    Update scripthome.js

--- js/scripthome.js
+++ js/scripthome.js
@@ -15,11 +15,20 @@ document.querySelector("#search-button").onclick = (e) => {
   e.preventDefault();
 };
 
+// Toggle class active untuk shopping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+  shoppingCart.classList.toggle('active');
+  e.preventDefault();
+};
+
 // klik diluar elemen
 const hm = document.querySelector("#humburger-menu");
 const sb = document.querySelector("#search-button");
+const sc = document.querySelector('#shopping-cart-button');
 
 document.addEventListener("click", function (e) {
+  console.log(e.target)
   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
@@ -27,4 +36,19 @@ document.addEventListener("click", function (e) {
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
   }
+
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
+  }
+});
+
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = 'flex';
+    e.preventDefault();
+  };
 });
